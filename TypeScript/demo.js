4dfd0e898096c87362a921daf634488c5ff60553
Shivam Gupta <112889717+GoldReigner@users.noreply.github.com>
2024-02-05 09:47:43 +0530

    02-02-2024

---
+++ TypeScript/demo.js
@@ -0,0 +1,10 @@
+var msg = 10;
+console.log(msg);
+console.log(typeof (msg));
+var isClass = true;
+console.log(typeof (isClass)); 
+
+function add(){
+    let l = 10;
+}
+console.log(i)
---
+++ TypeScript/demo.ts
@@ -0,0 +1,6 @@
+var msg:number = 10;
+console.log(msg);
+console.log(typeof(msg));
+
+var isClass:boolean = true;
+console.log(typeof(isClass))
